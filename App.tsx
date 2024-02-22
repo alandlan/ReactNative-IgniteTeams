5863b91ff76358bd774a854366e421516e170d8b
alandlan <alanhorcrux@gmail.com>
2024-02-22 16:57:22 -0300

    ajust status bar

--- App.tsx
+++ App.tsx
@@ -1,5 +1,6 @@
 import { useFonts,Roboto_400Regular,Roboto_700Bold } from "@expo-google-fonts/roboto";
 import { ThemeProvider } from "styled-components";
+import { StatusBar } from "react-native";
 
 import theme from "src/theme";
 import { Groups } from "@screens/groups";
@@ -14,6 +15,10 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
+      <StatusBar 
+        barStyle="light-content" 
+        backgroundColor="transparent"
+        translucent />
       { fontsLoaded ? <Groups /> : <Loading/> }
     </ThemeProvider>
   );
