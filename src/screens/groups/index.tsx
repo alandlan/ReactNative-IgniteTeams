7204acf1e067f5d8bbd88df5885caaeda728eb54
alandlan <alanhorcrux@gmail.com>
2024-02-22 18:02:15 -0300

    create component highlight

--- src/screens/groups/index.tsx
+++ src/screens/groups/index.tsx
@@ -1,10 +1,12 @@
 import { Header } from '@components/header';
 import {Container} from './styles';
+import { Highlight } from '@components/highlight';
 
 export function Groups() {
   return (
     <Container>
       <Header />
+      <Highlight title="Grupos" subTitle="Conheça os grupos disponíveis" />
     </Container>
   );
 }
