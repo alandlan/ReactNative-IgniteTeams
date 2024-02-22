84a8a300e39c769cf605b9f91c66d014d75b8824
alandlan <alanhorcrux@gmail.com>
2024-02-22 18:14:31 -0300

    create component groupcard

--- src/screens/groups/index.tsx
+++ src/screens/groups/index.tsx
@@ -1,12 +1,14 @@
 import { Header } from '@components/header';
-import {Container} from './styles';
 import { Highlight } from '@components/highlight';
+import { GroupCard } from '@components/groupcard';
+import {Container} from './styles';
 
 export function Groups() {
   return (
     <Container>
       <Header />
       <Highlight title="Grupos" subTitle="Conheça os grupos disponíveis" />
+      <GroupCard title="Grupo de Estudos" />
     </Container>
   );
 }
