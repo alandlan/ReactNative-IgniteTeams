5fc4a629cb5579279d6dbbcbc34546a1972398f8
alandlan <alanhorcrux@gmail.com>
2024-02-22 18:51:28 -0300

    create component button

--- src/screens/groups/index.tsx
+++ src/screens/groups/index.tsx
@@ -5,6 +5,7 @@ import {Container} from './styles';
 import { useState } from 'react';
 import { FlatList } from 'react-native';
 import { ListEmpty } from '@components/listempyt';
+import { Button } from '@components/button';
 
 export function Groups() {
   const [groups, setGroups] = useState<string[]>([]);
@@ -21,6 +22,8 @@ export function Groups() {
         contentContainerStyle={groups.length === 0 && {flex: 1}}
         ListEmptyComponent={() => <ListEmpty message="Nenhum grupo encontrado" />}
       />
+
+      <Button title="Criar grupo" />
       
     </Container>
   );
